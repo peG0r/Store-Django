f0bbda6db18159fc90a293e72854eebaf5e1eea3
peg0r <sebaslobos77@gmail.com>
2024-09-17 13:20:19 -0300

    vercel deploy 2

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 echo "Building the project..."
-python3.12.1 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 
 echo "Makr Migrations..."
 # Apply any outstanding database migrations
-python3.12.1 manage.py makemigrations --noinput
-python3.12.1 manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 
 # Convert static asset files
 echo "Collect Static..."
-python3.12.1 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
